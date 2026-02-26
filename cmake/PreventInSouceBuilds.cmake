6ef42857086f799e321629cabd1a99cd037c9166
kobtsev <2898815+kobtsev@users.noreply.github.com>
2026-02-26 14:59:50 +0300

    Create PreventInSouceBuilds.cmake

---
+++ cmake/PreventInSouceBuilds.cmake
@@ -0,0 +1,11 @@
+# ---- In-source guard ----
+
+include_guard(GLOBAL)
+
+if(CMAKE_SOURCE_DIR STREQUAL CMAKE_BINARY_DIR)
+  message(
+    FATAL_ERROR
+      "In-source builds are not supported. "
+      "You may need to delete 'CMakeCache.txt' and 'CMakeFiles/' first."
+  )
+endif()
